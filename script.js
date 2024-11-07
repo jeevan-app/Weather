827bca7d4e99cc77e3e4ce2a8075c539598165cf
jeevan <jeevan14.menthula>
2024-11-07 15:38:05 +0530

    initial

--- script.js
+++ script.js
@@ -44,10 +44,13 @@ async function updateweatherinfo(city){
 
     if(weatherdata.cod != 200) {
         showDisplaySection(notFoundSection);
-            function_five();
+
+        function_five();
         return;
     }
-    console.log(weatherdata);
+    // console.log(weatherdata);
+
+    notFoundSection.style.display = "none";
 
     const{
         name: country,
@@ -161,6 +164,5 @@ async function getfetchdata(endpoint, city){
 function function_five(){
     document.getElementById("last1").style.display = "none";
 }
-
 
 
